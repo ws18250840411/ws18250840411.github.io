74c1f8d660a2f495216e84104660e5c7fb92822e
wangws <wangws@akulaku.com>
2021-08-23 18:25:23 +0800

    feat: asd

---
+++ precache-manifest.ac45c59681b9e4ec8996ca203aaa1948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082e4eabd99dd29975672138acc57770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fea21aaa8f59e71b9b",
+    "url": "/static/css/main.b102c039.chunk.css"
+  },
+  {
+    "revision": "4f007675ffba6c6f9f5f",
+    "url": "/static/js/2.fbbe080c.chunk.js"
+  },
+  {
+    "revision": "e82c8c794923bdad70a86a257832ddaf",
+    "url": "/static/js/2.fbbe080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fea21aaa8f59e71b9b",
+    "url": "/static/js/main.e850f108.chunk.js"
+  },
+  {
+    "revision": "63e5a3bc28ba5864fa19",
+    "url": "/static/js/runtime-main.4b4a3fe6.js"
+  }
+]);
